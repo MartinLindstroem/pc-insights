32e134c0ba64303c3f89c1827afc6c2c627ac51c
Martin Lindström <martinllindstroem@gmail.com>
2024-12-28 15:38:40 +0100

    remove comments

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { app, BrowserWindow, ipcMain } from "electron";
 import path from "path";
 import started from "electron-squirrel-startup";
 import { getSystemInfo } from "./hooks/systemInfo";
-import { get } from "http";
-import { system } from "systeminformation";
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
@@ -17,19 +15,15 @@ const createWindow = () => {
     height: 720,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
-      // nodeIntegration: true,
-      // contextIsolation: true,
     },
   });
 
-  // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
     mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
   }
 
-  // Open the DevTools.
   mainWindow.webContents.openDevTools();
 };
 
@@ -59,6 +53,3 @@ ipcMain.handle("getSystemInfo", async () => {
   const systemInfo = await getSystemInfo();
   return systemInfo;
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
